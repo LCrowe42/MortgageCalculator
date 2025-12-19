b6a354b5386f11b08e52ffbc4ab3a0b50d559c6a
Skywalker12500 <142610370+Skywalker12500@users.noreply.github.com>
2025-12-19 04:07:31 -0500

    Created a script.js with a calculate function which modifies the html elements

---
+++ mortgage-repayment-calculator-main/script.js
@@ -0,0 +1,15 @@
+function calculate() {
+    const amount = document.getElementById("amount").value;
+    const term = document.getElementById("term").value;
+    const rate = document.getElementById("rate").value / 100 / 12;
+    const interestOnly = document.getElementById("interestonly").checked;
+
+    const numberOfPayments = term * 12;
+    const repayments = amount * (rate * (1 + rate) ** numberOfPayments) / ((1 + rate) ** numberOfPayments - 1);
+    const total = repayments * numberOfPayments;
+
+    const repaymentsElement = document.getElementById("monthlypayments");
+    repaymentsElement.textContent = repayments.toFixed(2).toString();
+    const totalElement = document.getElementById("total");
+    totalElement.textContent = total.toFixed(2).toString();
+}
