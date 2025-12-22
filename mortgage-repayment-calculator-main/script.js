e49b2e015afc66301cf93ec0b6336032f155b68c
Skywalker12500 <142610370+Skywalker12500@users.noreply.github.com>
2025-12-22 03:15:57 -0500

    Major changes to empty and filled results Formatted output to currency added hover css attributes

--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -1,12 +1,14 @@
 function generateResultsRepayment(repayments, total) {
-    return ' \
-        <p class="otitle">Your results</p> \
-        <p class="obody">Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p> \
-        <div class="obox"> \
-            <output class="monthlypayments">${repayments}</output> \
-            <output class="total">${total}</output> \
-        </div> \
-    ';
+    let repstring = repayments.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    let totstring = total.toLocaleString("en-US", {style:"currency", currency:"USD"});
+    return `
+        <p class="otitle">Your results</p>
+        <p class="obody">Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+        <div class="obox">
+            <output class="monthlypayments">${repstring}</output>
+            <output class="total">${totstring}</output>
+        </div>
+    `;
 }
 
 function calculate() {
@@ -17,8 +19,8 @@ function calculate() {
     
     if (!interestOnly) {
         const numberOfPayments = term * 12;
-        const repayments = (amount * (rate * (1 + rate) ** numberOfPayments) / ((1 + rate) ** numberOfPayments - 1)).toFixed(2).toString();
-        const total = (repayments * numberOfPayments).toFixed(2).toString();
+        const repayments = Number((amount * (rate * (1 + rate) ** numberOfPayments) / ((1 + rate) ** numberOfPayments - 1)).toFixed(2));
+        const total = Number((repayments * numberOfPayments).toFixed(2));
 
         const resultsContainer = document.getElementById("results");
         resultsContainer.innerHTML = generateResultsRepayment(repayments, total);
